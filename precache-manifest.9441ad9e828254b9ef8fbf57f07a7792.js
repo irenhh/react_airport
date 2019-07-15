ee73a52e96b8391b58c377067286effa870191c2
Iryna <iriskag@ukr.net>
2019-07-15 17:47:48 +0300

    Updates

---
+++ precache-manifest.9441ad9e828254b9ef8fbf57f07a7792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97767aaa9e8aac22cd858eed84ae8f27",
+    "url": "/react_airport/index.html"
+  },
+  {
+    "revision": "03e8e49edd68a61d7284",
+    "url": "/react_airport/static/css/main.d7dad8d9.chunk.css"
+  },
+  {
+    "revision": "767e158467df4297e433",
+    "url": "/react_airport/static/js/2.8515f2cd.chunk.js"
+  },
+  {
+    "revision": "03e8e49edd68a61d7284",
+    "url": "/react_airport/static/js/main.330ca79a.chunk.js"
+  },
+  {
+    "revision": "c8cd8e70b1c2d1cd9765",
+    "url": "/react_airport/static/js/runtime~main.037fa8df.js"
+  }
+]);
